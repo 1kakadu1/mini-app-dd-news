25afe23b6d5f8216597146acfc93ca3c926e8637
Anton <a.soroka@terabit.ai>
2025-06-19 16:00:18 +0300

    useeffect fullscreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
-function App() {
+import { isFullscreen, requestFullscreen } from "@telegram-apps/sdk";
+import { useEffect, useState } from "react";
 
+
+function App() {
+  const [error, setError] = useState("");
+  const fullscreen = async ()=>{
+      if (requestFullscreen.isAvailable()) {
+       try {
+         await requestFullscreen();
+         setError(`Fullscreen requested: ${isFullscreen()}`)
+         console.log("Fullscreen requested:", isFullscreen()); // Check if it's actually fullscreen
+       } catch (error) {
+          setError(JSON.stringify(error))
+          console.error("Failed to request fullscreen:", error);
+       }
+     } else {
+       setError("Fullscreen is not available")
+       console.warn("Fullscreen is not available");
+     }
+}
+  useEffect(()=>{
+    fullscreen();
+  }, [])
   return (
     <>
       <div
@@ -9,6 +31,7 @@ function App() {
           minHeight: "100vh",
         }}
       >
+        {<h1>INFO: {error}</h1>}
         <iframe className="site" src="https://dd.news/" />
       </div>
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { init, miniApp, expandViewport , requestFullscreen} from "@telegram-apps/sdk-react";
+import { init, miniApp, expandViewport, setDebug} from "@telegram-apps/sdk-react";
 import './mockEnv.ts';
 const root = createRoot(document.getElementById('root')!);
 
 const initializeTelegramSDK = async () => {
   try {
+    setDebug(false)
     await init();
     if (miniApp.ready.isAvailable()) {
       await miniApp.ready();
-    }
-    if (requestFullscreen.isAvailable()) {
-      await requestFullscreen();
     }
 
     if (expandViewport.isAvailable()) {
