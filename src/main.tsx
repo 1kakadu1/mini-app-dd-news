6c61bd70366933fa0210c26500605f0579f920cd
Anton <a.soroka@terabit.ai>
2025-06-19 17:35:48 +0300

    test vieport

--- src/main.tsx
+++ src/main.tsx
@@ -1,43 +1,54 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { init, expandViewport, requestFullscreen, miniApp } from "@telegram-apps/sdk-react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import {
+  init,
+  expandViewport,
+  miniApp,
+  viewport,
+} from "@telegram-apps/sdk-react";
 //import './mockEnv.ts';
-const root = createRoot(document.getElementById('root')!);
+const root = createRoot(document.getElementById("root")!);
 
 const initializeTelegramSDK = async () => {
   try {
-
     init();
-    expandViewport();
-    void import('eruda').then(({ default: eruda }) => {
+
+    void import("eruda").then(({ default: eruda }) => {
       eruda.init();
       eruda.position({ x: window.innerWidth - 50, y: 0 });
     });
-      
-    if (requestFullscreen.isAvailable()) {
-        await requestFullscreen();
-    } 
+
+    if (viewport.mount.isAvailable()) {
+      viewport.mount();
+    }
+
+    if (viewport.expand.isAvailable()) {
+      viewport.expand();
+    }
+
+    if (viewport.requestFullscreen.isAvailable()) {
+      await viewport.requestFullscreen();
+    }
 
     if (miniApp.mountSync.isAvailable()) {
-      await miniApp.mountSync();
+        miniApp.mountSync();
     }
 
-    if(miniApp.setHeaderColor.isAvailable()){
+    if (miniApp.setHeaderColor.isAvailable()) {
       miniApp.setHeaderColor("#ebeff2");
     }
+
   } catch (error) {
     console.error("Ошибка инициализации:", error);
   }
 };
 
-initializeTelegramSDK().then(()=>{
-      root.render(
-        <StrictMode>
-         <App/>
-        </StrictMode>,
-      );
+initializeTelegramSDK().then(() => {
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
 });
-
-
