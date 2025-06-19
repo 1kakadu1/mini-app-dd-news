11f77a91aaf2fde78d4c04eaabe2665dcec6d5a4
Anton <a.soroka@terabit.ai>
2025-06-19 16:32:48 +0300

    test colors

--- src/main.tsx
+++ src/main.tsx
@@ -2,26 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { init, miniApp, setDebug,isFullscreen, requestFullscreen, expandViewport} from "@telegram-apps/sdk-react";
+import { init, miniApp, setDebug, expandViewport} from "@telegram-apps/sdk-react";
 import './mockEnv.ts';
 const root = createRoot(document.getElementById('root')!);
 
 const initializeTelegramSDK = async () => {
   try {
-    setDebug(false)
+    setDebug(true)
     await init();
 
     if(expandViewport.isAvailable()){
       expandViewport();
     }
     
-    if (requestFullscreen.isAvailable()) {
-        await requestFullscreen();
-        console.log("Fullscreen requested:", isFullscreen()); // Check if it's actually fullscreen
-    } 
+    // if (requestFullscreen.isAvailable()) {
+    //     await requestFullscreen();
+    //     console.log("Fullscreen requested:", isFullscreen()); // Check if it's actually fullscreen
+    // } 
 
     if (miniApp.ready.isAvailable()) {
       await miniApp.ready();
+      miniApp.setHeaderColor("red");
+      miniApp.setBackgroundColor("green")
     }
   } catch (error) {
     console.error("Ошибка инициализации:", error);
