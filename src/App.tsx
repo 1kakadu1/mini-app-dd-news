0ebe4cc53bee5c40aace5b8afd6bddbf8d560583
Anton <1kakady@gmail.com>
2025-06-19 23:19:00 +0300

    test size iframe

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,28 @@ import { viewport } from "@telegram-apps/sdk-react";
 import { useMemo } from "react";
 
 function App() {
-  const size = useMemo(()=>{
-   const safe = viewport.safeAreaInsets();
-   const  content = viewport.contentSafeAreaInsets();
+  const size = useMemo(() => {
+    const safe = viewport.safeAreaInsets();
+    const content = viewport.contentSafeAreaInsets();
 
-   return{
-    top: Math.ceil(content.top === 0 ? safe.top : content.top),
-    bottom: Math.ceil(safe.bottom === 0 ? content.bottom : safe.bottom),
-   }
-  },[viewport]);
-  
+    return {
+      top: Math.ceil(content.top === 0 ? safe.top : content.top),
+      bottom: Math.ceil(safe.bottom === 0 ? content.bottom : safe.bottom),
+    };
+  }, [viewport]);
+
   return (
     <>
       <div
         className="main"
-        style={{
-          paddingTop: size.top,
-          height: `calc(100vh - ${size.bottom}px)`
-        }}
-      > 
-        <iframe className="site" src="https://dd.news/" />
+      >
+        <iframe
+          style={{
+            height: `calc(100vh - ${size.bottom+size.top})`,
+          }}
+          className="site"
+          src="https://dd.news/"
+        />
       </div>
     </>
   );
