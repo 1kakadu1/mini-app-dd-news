35763318a20ef2b36d3be98614a70767342e9a71
Anton <a.soroka@terabit.ai>
2025-06-19 17:24:08 +0300

    test

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { init, expandViewport, requestFullscreen } from "@telegram-apps/sdk-react";
+import { init, expandViewport, requestFullscreen, miniApp } from "@telegram-apps/sdk-react";
 //import './mockEnv.ts';
 const root = createRoot(document.getElementById('root')!);
 
@@ -20,9 +20,13 @@ const initializeTelegramSDK = async () => {
         await requestFullscreen();
     } 
 
-    // if (miniApp.ready.isAvailable()) {
-    //   await miniApp.;
-    // }
+    if (miniApp.mountSync.isAvailable()) {
+      await miniApp.mountSync();
+    }
+
+    if(miniApp.setHeaderColor.isAvailable()){
+      miniApp.setHeaderColor("#ebeff2");
+    }
   } catch (error) {
     console.error("Ошибка инициализации:", error);
   }
